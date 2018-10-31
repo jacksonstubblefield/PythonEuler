8dfbec0d0c811a0cbd01cf5ffd18cfd7781339b1
Jackson Stubblefield <jrstubb@gmail.com>
2018-10-31 12:36:50 -0500

    Create euler3_1.py

---
+++ euler3_1.py
@@ -0,0 +1,34 @@
+# What is the largest prime factor of the number 600851475143 ?
+# Answer: 6857
+
+primes = [2]
+z = 600851475143
+i = 1
+while i < z:
+    if i > primes[len(primes)-1]:
+        isPrime = True
+        for p in primes:
+            if i % p == 0:
+                isPrime = False
+        if isPrime == True:
+            primes.append(i)
+            if z % i == 0:
+                z /= i
+                i = 1
+    else:
+        for p in primes:
+            if i == p:
+                if z % i == 0:
+                    z /= i
+                    i = 1
+    i += 1
+print(z)
+
+# The idea behind this is to teach the program how to identify and keep track of prime numbers as we iterate upwards.
+# z is large and to keep it from taking too much time, we want to only deal with small iterations.
+# Therefore the strategy is to strip away the lowest prime factors we're able to find until we're left with the largest prime factor -
+# For which any i, it's unable to divide fully.
+# We split it up into two forms to minimize time taken: whether we need to treat it as potential addention to our prime list or whether
+# to identify whether or not i is prime and whether it's able to be stripped from z.
+
+# Calculation time (on my PC): 0.3124
