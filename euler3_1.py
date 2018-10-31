de72f45d58df5efdce40eccbf7056627b8cb4dca
Jackson Stubblefield <jrstubb@gmail.com>
2018-10-31 12:39:59 -0500

    Update euler3_1.py

--- euler3_1.py
+++ euler3_1.py
@@ -24,11 +24,12 @@ while i < z:
     i += 1
 print(z)
 
+# The objective is to strip prime factors from z in order to find the largest irreducible factor of z as quickly as we can.
 # The idea behind this is to teach the program how to identify and keep track of prime numbers as we iterate upwards.
 # z is large and to keep it from taking too much time, we want to only deal with small iterations.
 # Therefore the strategy is to strip away the lowest prime factors we're able to find until we're left with the largest prime factor -
-# For which any i, it's unable to divide fully.
-# We split it up into two forms to minimize time taken: whether we need to treat it as potential addention to our prime list or whether
-# to identify whether or not i is prime and whether it's able to be stripped from z.
+# for which any i, it's unable to divide fully.
+# We split it up into two forms to minimize time taken: whether we need to treat it as potential addition to our prime list or whether
+# to identify whether or not i is already listed and whether it's able to be stripped from z.
 
 # Calculation time (on my PC): 0.3124
