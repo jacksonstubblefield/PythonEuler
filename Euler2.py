a6dbf1de58b8c999de4eac29c8329636666cdc0a
Jackson Stubblefield <jrstubb@gmail.com>
2018-10-30 22:56:52 -0500

    Update Euler2.py

--- Euler2.py
+++ Euler2.py
@@ -9,3 +9,6 @@ while z < 4000000:
     if z % 2 == 0:
         s += z
 print(s)
+
+# Here we slide the values back one position each iteration of the While loop - saving the even values as we go.
+# Ideally we wouldn't have to be so explicit with our redefinitions - if you can generalize this, get in touch.
