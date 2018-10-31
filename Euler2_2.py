14cbd1c50efdf0cb81d0b23963f437e7731fb2d7
Jackson Stubblefield <jrstubb@gmail.com>
2018-10-30 23:15:52 -0500

    Update Euler2_2.py

--- Euler2_2.py
+++ Euler2_2.py
@@ -10,4 +10,4 @@ print (s)
 # This requires one to note that an even is the sum of two odd numbers.  An odd plus an even must be odd.  Insinuating even numbers
 # come along every third term.  This can be manipulated.
 # The sequence goes as follows: x, y, x+y, x+2y, 2x+3y  ...  The last two terms are defined in terms of the first two terms.
-# Thanks to PE user Begoner for the explanation of symmetry.
+# Thanks to PE user Begoner for pointing the O, O, E pattern out.
