0b800d47b1a53cbd6f08eff547769d16179db7fe
Jackson Stubblefield <jrstubb@gmail.com>
2018-10-30 22:39:17 -0500

    Update Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -8,7 +8,3 @@ for i in range(1000):
     elif i % 5 == 0:
         f = f + i
 print(f)
-
-# In Python we practice brevity - no object identification, no hassle
-# We count up from 0 to 1000 and chuck the int in the sum if it meets either of the following criteria:
-# the integer divides 3 cleanly or the integer divides 5 cleanly
